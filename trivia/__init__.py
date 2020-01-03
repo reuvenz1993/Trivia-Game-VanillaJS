2a37b4a6ab138b23b4fc0c49adc0cc1cafa9f045
reuvenz1993 <rovenroven1@gmail.com>
2020-01-03 05:30:14 +0200

    socketio

--- trivia/__init__.py
+++ trivia/__init__.py
@@ -8,10 +8,11 @@ from flask_socketio import SocketIO
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecretkey'
 socketio = SocketIO(app)
 
 # Often people will also separate these into a separate config.py file
-app.config['SECRET_KEY'] = 'mysecretkey'
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
