b9e903e0d5070946d2b082b1e3d9934547fb75f3
reuvenz1993 <rovenroven1@gmail.com>
2020-01-03 07:02:16 +0200

    --- Auto-Git Commit ---

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from trivia import app, db, socketio
 import trivia.views
-from flask_socketio import emit
+from flask_socketio import emit, send
 import json
 
 @socketio.on('connect')
@@ -18,20 +18,31 @@ def handle_message(message):
     r = message
     print (r)
     print ('handle_message function run')
-    ar = json.loads(message)
-    print (ar)
+    try:
+        ar = json.loads(message)
+        print (ar)
+    except:
+        print("not json")
     print('received message: ' + message)
+    send(message)
 
 
 @socketio.on('json')
 def handle_json(json):
+    try:
+        jsondata = json
+        print (jsondata)
+    except:
+        jsondata_2 = json.loads(json)
     print('received json: ' + str(json))
+    return 'one', 2
 
 
 
-@socketio.on('my event')
+@socketio.on('my_event')
 def handle_my_custom_event(data):
     print('received args: ' + data)
+    send(data)
 
 
 if __name__ == '__main__':
